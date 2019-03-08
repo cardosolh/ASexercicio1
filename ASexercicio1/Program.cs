9a2a282c550464e1c48e38ae9f0ebc7f3b3477e5
Luiz H. Cardoso <c.luizhenrique@gmail.com>
2019-03-07 22:04:22 -0300

    primeiro commit

---
+++ ASexercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASexercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
